089855be32b4d1fe9689eff7713a0efd75ff5828
zeuros <13236310+zeuros@users.noreply.github.com>
2022-11-29 12:50:54 +0100

    look n feel

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ import './style.css';
 const IMAGE_WIDTH = 400;
 const IMAGE_HEIGHT = 400;
 
-function zoomed(evt) {
-  const svg = d3.select('svg');
-  const wrapper = d3.select('#wrapper').node();
+const zoomed = (wrapper, svg) => (evt) => {
   const scale = evt.transform.k;
-
   const scaledWidth = IMAGE_WIDTH * scale;
   const scaledHeight = IMAGE_HEIGHT * scale;
 
@@ -20,31 +17,33 @@ function zoomed(evt) {
   svg.select('g').attr('transform', `scale(${scale})`);
 
   // Move scrollbars.
-  wrapper.scrollLeft = -evt.transform.x;
-  wrapper.scrollTop = -evt.transform.y;
+  wrapper.node().scrollLeft = -evt.transform.x;
+  wrapper.node().scrollTop = -evt.transform.y;
 
   // If the image is smaller than the wrapper, move the image towards the
   // center of the wrapper.
-  const dx = d3.max([0, wrapper.clientWidth / 2 - scaledWidth / 2]);
-  const dy = d3.max([0, wrapper.clientHeight / 2 - scaledHeight / 2]);
-  d3.select('svg').attr('transform', `translate(${dx}, ${dy})`);
-}
+  const dx = d3.max([0, wrapper.node().clientWidth / 2 - scaledWidth / 2]);
+  const dy = d3.max([0, wrapper.node().clientHeight / 2 - scaledHeight / 2]);
+  svg.attr('transform', `translate(${dx}, ${dy})`);
+};
 
-function scrolled() {
-  const wrapper = d3.select('#wrapper');
+const scrolled = (wrapper) => (evt) => {
   const x = wrapper.node().scrollLeft + wrapper.node().clientWidth / 2;
   const y = wrapper.node().scrollTop + wrapper.node().clientHeight / 2;
   const scale = d3.zoomTransform(wrapper.node()).k;
   // Update zoom parameters based on scrollbar positions.
   wrapper.call(d3.zoom().translateTo, x / scale, y / scale);
-}
+};
+
+const wrapper = d3.select('#wrapper');
+const svg = d3.select('svg');
 
 // Set initial SVG dimensions.
-d3.select('svg').attr('width', IMAGE_WIDTH).attr('height', IMAGE_HEIGHT);
+svg.attr('width', IMAGE_WIDTH).attr('height', IMAGE_HEIGHT);
 
 // Set up d3-zoom and callbacks.
 d3.select('#wrapper')
-  .on('scroll', scrolled)
+  .on('scroll', scrolled(wrapper))
   .call(
     d3
       .zoom()
@@ -53,5 +52,5 @@ d3.select('#wrapper')
         [0, 0],
         [IMAGE_WIDTH, IMAGE_HEIGHT],
       ])
-      .on('zoom', zoomed)
+      .on('zoom', zoomed(wrapper, svg))
   );
